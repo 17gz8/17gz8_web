5bd87c6a2cc387c5dd808376b0e1b5694ccaaac9
Caval1er <Caval1er>
2021-01-24 23:30:35 +0800

    [feat]封装人物信息卡片

--- pages/index/task/_id.vue
+++ pages/index/task/_id.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    耕种详情12
+  <div class="container">
+    <div class="task-head">
+      <div class="task-text">
+        <div class="task-text-container">
+          <div class="task-text-title">
+            <h2>{{ task.title }}</h2>
+            <p>{{ task.position }}</p>
+          </div>
+          <div class="task-text-icon" />
+        </div>
+        <PersonInfo />
+      </div>
+      <div class="task-video" />
+    </div>
+    <div class="timeline" />
+    <div class="task-info" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      task: {
+        title: '播种豌豆',
+        position: '希望小村菜地'
+      }
+    }
+  }
+}
 </script>
 
 <style  scoped></style>
