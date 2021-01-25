86cbeda85be85794b916aecb119020bc7b90cb97
hotyan <hotyann@163.com>
2021-01-25 15:25:28 +0800

    [feat]:新增git hooks

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,17 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    transpile: [/^element-ui/]
+    transpile: [/^element-ui/],
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
