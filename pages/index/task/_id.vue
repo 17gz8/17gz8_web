3f25fae4d5a8d5f66c4566ca9610048ef48a3081
Caval1er <Caval1er>
2021-01-26 16:28:09 +0800

    [fix]输入假数据

--- pages/index/task/_id.vue
+++ pages/index/task/_id.vue
@@ -2,7 +2,7 @@
   <div class="task-detail">
     <el-row>
       <el-col :span="6">
-        <div class="grid-content bg-purple-dark">
+        <div>
           <el-card class="box-card">
             <div slot="header" class="clearfix">
               <span class="task-title">播种豌豆</span>
@@ -10,21 +10,24 @@
             </div>
             <div class="text item">
               <span class="task-position">希望小村菜地</span>
-              <el-button type="danger" plain>
+              <el-button type="danger">
                 加入任务
               </el-button>
             </div>
           </el-card>
           <el-card>
             <div class="person-info">
-              <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" shape="circle" :size="120" />
+              <el-avatar :src="task.profile.avatarSrc" shape="circle" :size="120" />
               <div class="card-info">
                 <div>
                   <h2 class="person-title">
-                    重庆市渝中小学
+                    {{ task.profile.school }}
                   </h2>
                 </div>
-                <div><span class="person-name">刘老师</span> <span class="person-time">2天前</span></div>
+                <div>
+                  <span class="person-name"> {{ task.profile.name }}</span>
+                  <span class="person-time">{{ task.profile.time }}天前</span>
+                </div>
               </div>
             </div>
           </el-card>
@@ -48,34 +51,29 @@
         </div>
       </el-col>
       <el-col :span="18">
-        <div class="grid-content bg-purple">
+        <div>
           视频组件待封装
         </div>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="24">
-        <div class="grid-content bg-purple">
-          <h2>任务时间轴</h2>
+        <div class="timeline">
+          <h2 class="timeline-title">
+            任务时间轴
+          </h2>
         </div>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="8">
-        <div class="grid-content bg-purple" />
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple-light" />
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple" />
+      <el-col :span="24">
+        <div />
       </el-col>
     </el-row>
     <el-row class="task-footer">
       <el-col :span="24">
-        <div class="grid-content bg-purple">
-          <span>同类任务</span>
-          <span>菜地历史</span>
+        <div>
+          <span v-for="(item,index) in taskCategory" :key="item" :class="[index === 0 ? 'task-same' : '', index === 1 ? 'task-history' : '', currentIndex === index? 'active' : '']" @click="isClick(index)">{{ item }}</span>
         </div>
       </el-col>
     </el-row>
@@ -87,13 +85,19 @@
               <div class="card-img">
                 <el-image :src="url" style="width: 160px; height: 160px" />
               </div>
-              <div>
-                <h2>玫瑰花种植</h2>
+              <div class="card-right">
+                <h2 class="task-info-title">
+                  玫瑰花种植
+                </h2>
                 <div class="task-info-text">
-                  <el-avatar :src="url" shape="circle" :size="20" />
-                  <div>
-                    <h2>刘老师</h2>
-                    <p>新华小学</p>
+                  <el-avatar :src="url" shape="circle" :size="60" />
+                  <div class="task-info-text-wrap2">
+                    <h2 class="task-info-person">
+                      刘老师
+                    </h2>
+                    <p class="task-info-position">
+                      新华小学
+                    </p>
                   </div>
                 </div>
               </div>
@@ -109,9 +113,21 @@
 export default {
   data () {
     return {
+      currentIndex: 0,
       task: {
         title: '播种豌豆',
-        position: '希望小村菜地'
+        position: '希望小村菜地',
+        // 简介
+        profile: {
+          avatarSrc: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+          school: '重庆市渝北小学',
+          name: '刘老师',
+          time: '2'
+        }
+        // 任务描述
+        // description:{
+
+        // }
       },
       urls: [
         'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
@@ -121,8 +137,13 @@ export default {
         'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
         'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
         'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
-      ]
-
+      ],
+      taskCategory: ['同类任务', '菜地历史']
+    }
+  },
+  methods: {
+    isClick (index) {
+      this.currentIndex = index
     }
   }
 }
@@ -135,6 +156,7 @@ export default {
 }
 .task-detail{
   margin-left:100px;
+  font-family: Source Han Sans CN;
 }
 .el-row {
     margin-bottom: 20px;
@@ -144,15 +166,6 @@ export default {
   }
   .el-col {
     border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
   }
   .grid-content {
     border-radius: 4px;
@@ -200,7 +213,7 @@ export default {
 
   .card-box{
     width:402px;
-    height: 160px;
+    /* height: 160px; */
     margin:0px 40px 52px 0px;
   }
 
@@ -217,17 +230,16 @@ export default {
     /* width: 192px;
     height: 48px; */
     font-size: 48px;
-    font-family: Source Han Sans CN;
     font-weight: bold;
-    line-height: 82px;
     color: #707070;
+    padding-left: 18px;
+    border-left: 8px solid #FF6262;
   }
 
   .task-position{
     /* width: 216px;
     height: 36px; */
     font-size: 36px;
-    font-family: Source Han Sans CN;
     font-weight: 400;
     line-height: 61px;
     color: #666666;
@@ -238,7 +250,6 @@ export default {
     height: 36px; */
     margin-top:20px;
     font-size: 36px;
-    font-family: Source Han Sans CN;
     font-weight: 500;
     line-height: 61px;
     color: #666666;
@@ -247,7 +258,6 @@ export default {
 
   .person-name{
     font-size: 24px;
-    font-family: Source Han Sans CN;
     font-weight: 500;
     line-height: 41px;
     color: #666666;
@@ -256,7 +266,6 @@ export default {
   .person-time{
     margin-left:20px;
     font-size: 24px;
-    font-family: Source Han Sans CN;
     font-weight: 400;
     line-height: 41px;
     color: #999999;
@@ -268,7 +277,6 @@ export default {
 
   .task-startTime{
     font-size: 24px;
-    font-family: Source Han Sans CN;
     font-weight: 400;
     line-height: 41px;
     color: #999999;
@@ -276,9 +284,76 @@ export default {
 
   .task-text{
     font-size: 24px;
-    font-family: Source Han Sans CN;
     font-weight: 400;
     line-height: 41px;
     color: #999999;
+  }
+
+  .task-info-title{
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 41px;
+    color: #666666;
+  }
+
+  .task-info-text{
+    display: flex;
+    margin-top:15px;
+  }
+
+  .card-right{
+    margin: 20px 0 0 23px;
+  }
+
+  .task-info-person{
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 35px;
+    color: #999999;
+    /* margin-bottom:8px ; */
+  }
+
+  .task-info-position{
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 27px;
+    color: #999999;
+  }
+
+  .task-info-text-wrap2{
+    margin-left:10px;
+  }
+
+  .task-same,.task-history{
+  font-size: 36px;
+  font-weight: 400;
+  color: #666666;
+  cursor: pointer;
+  }
+
+  .task-same{
+    padding-left: 18px;
+    border-left: 8px solid #FF6262;
+    margin-right:40px ;
+  }
+
+  .timeline-title{
+    padding-left: 18px;
+    border-left: 8px solid #FF6262;
+    font-size: 48px;
+    font-weight: bold;
+    color: #666666;
+  }
+
+  .el-button{
+    font-size:24px;
+    padding: 6px 12px;
+    float: right;
+    margin-top: 14px;
+  }
+
+  .active{
+    font-size: 48px;
+    font-weight: bold;
   }
   </style>
