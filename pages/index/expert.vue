da42149174550acae64cdbdafcf864456c1ad8de
cheerddd <2664330029@qq.com>
2021-01-27 00:19:58 +0800

    完善专家介绍页面/新增菜地详情静态页面

--- pages/index/expert.vue
+++ pages/index/expert.vue
@@ -1,42 +1,43 @@
 <template>
-  <div class="container">
-    <div class="leftContent">
-      <el-image :src="imgUrl" class="ex-img" />
+  <el-card class="bigBox">
+    <div class="container">
+      <div class="leftContent">
+        <el-image :src="imgUrl" class="ex-img" />
+      </div>
+      <div class="rightContent">
+        <div id="experIntro" class="experIntro">
+          <h2>汪明荃</h2>
+          <h3>陕西农业大学教授</h3>
+          <p>农作物的生长特效，病理研究</p>
+        </div>
+        <div id="detailIntro" class="detailIntro">
+          <div class="context">
+            <el-image :src="imgUrl2" class="littleimg" />
+            <p>
+              现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士
+            </p>
+          </div>
+          <div class="imgLists">
+            <el-image :src="img100" class="img100" />
+            <el-image :src="img101" class="img101" />
+            <el-image :src="img100" class="img102" />
+            <el-image :src="img100" class="img103" />
+          </div>
+        </div>
+        <div class="experList">
+          <el-image :src="img009" class="helpImg" />
+          <el-image :src="img010" class="helpText" />
+          <div class="experImgs">
+            <el-image :src="img007" class="helpLeft" />
+            <div v-for="(item,index) in imgs" :key="index" class="imgs">
+              <el-image :src="item.url" class="exper01" />
+            </div>
+            <el-image :src="img008" class="helpRight" />
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="rightContent">
-      <div id="experIntro" class="experIntro">
-        <h2>汪明荃</h2>
-        <h3>陕西农业大学教授</h3>
-        <p>农作物的生长特效，病理研究</p>
-      </div>
-      <div id="detailIntro" class="detailIntro">
-        <div class="context">
-          <el-image :src="imgUrl2" class="littleimg" />
-          <p>
-            现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士
-          </p>
-        </div>
-        <div class="imgLists">
-          <el-image :src="img100" class="img100" />
-          <el-image :src="img101" class="img101" />
-          <el-image :src="img100" class="img102" />
-          <el-image :src="img100" class="img103" />
-        </div>
-      </div>
-      <div class="experList">
-        <el-image :src="img009" class="helpImg" />
-        <el-image :src="img010" class="helpText" />
-        <div class="experImgs">
-          <el-image :src="img007" class="helpLeft" />
-          <el-image :src="img006" class="exper01" />
-          <el-image :src="img004" class="exper02" />
-          <el-image :src="img005" class="exper03" />
-          <el-image :src="img003" class="exper04" />
-          <el-image :src="img008" class="helpRight" />
-        </div>
-      </div>
-    </div>
-  </div>
+  </el-card>
 </template>
 <script>
 export default {
@@ -50,20 +51,31 @@ export default {
       img003: require('../../static/images/003.png'),
       img004: require('../../static/images/004.png'),
       img005: require('../../static/images/00x.png'),
-      img006: require('../../static/images/006.png'),
       img008: require('../../static/images/008.png'),
       img100: require('../../static/images/100.png'),
-      img101: require('../../static/images/101.png')
+      img101: require('../../static/images/101.png'),
+      imgs: [
+        { url: require('../../static/images/006.png') },
+        { url: require('../../static/images/004.png') },
+        { url: require('../../static/images/005.png') },
+        { url: require('../../static/images/003.png') }
+      ]
     }
   }
 }
 </script>
 <style scoped>
+.bigBox{
+  width:90%;
+  margin:2% 5%;
+  height:740px;
+}
 .container{
   position: relative;
-  width:100vw;
+  width:100%;
   height:700px;
-  padding:50px 200px 40px 150px;
+  /* padding:20px 200px 40px 150px; */
+  padding:5% 5%;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -76,8 +88,8 @@ export default {
   width:100%;
 }
 .ex-img{
-  height:650px;
-  width:470px;
+  width:100%;
+  height:100%;
 }
 .rightContent{
   flex:1.2;
@@ -92,12 +104,13 @@ export default {
 flex:1;
 width:100%;
 margin-bottom:10px;
+position: relative;
 }
 .experIntro h2{
   color:#666666;
   font:Source Han Sans CN bold 80px;
   border-left:5px #FF6262 solid;
-  padding:0 10px;
+  padding:0 1%;
   position:relative;
   left:-110px;
   text-align:left;
@@ -145,39 +158,41 @@ margin-bottom:10px;
   height:10px;
 }
 .littleimg{
-  width:39px;
-  height:32px;
+  width:8%;
+  height:9%;
   margin-bottom:10px;
 }
 .experList{
   flex:1;
-  width:120%;
+  width:100%;
   margin-top:10px;
 }
 .experList .helpImg{
-  width:60px;
-  height:60px;
+  width:12%;
+  height:20%;
 }
 .experList .helpText{
-  width:96px;
-  height:24px;
+  width:12%;
+  height:8%;
   position: relative;
-  top:-12px;
+  top:-3%;
 }
 .experList .experImgs{
-  transform:translateX(-8%);
+  transform:translateX(-3%);
   display:flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  column-gap: 15px;
-  margin-top:10px;
-}
-.exper01,.exper02,.exper03,.exper04{
-width:120px;
-height:120px;
-cursor:pointer;
+  column-gap: 3%;
+  margin-top:20px;
 }
 .helpLeft,.helpRight{
+  flex:1;
   cursor:pointer;
+}
+.experList .experImgs .imgs{
+  flex:4;
+  display:flex;
+  flex-direction:row;
 }
 </style>
--- pages/index/garden/_gardenid/_fieldid.vue
+++ pages/index/garden/_gardenid/_fieldid.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="container">
-    菜地{{ fieldid }}详情
+    <div id="left">
+      <div id="content">
+        <div id="content_title" class="title">{{ fieldid }}号菜地</div>
+        <el-card shadow="never" id="content_text">
+          希望小村菜地距离重庆时钟线约有巫师公里，栈底面积5000亩（
+          主题参观去为400亩）。是国家4A级旅游景区、全国农业旅游示范点、
+          上海市科普教育基地。
+        </el-card>
+        <div id="content_img">
+          <div v-for="(item,index) in picture" :key="index" class="img">
+            <el-image :src="imgs" style="width:100%;height:100%"></el-image>
+          </div>
+        </div>
+        <div id="img_icon">
+          <el-radio-group v-model="radio" fill="#F56C6C">
+            <span style="margin-left:3px"><el-radio :label="none" class="radio" fill="#F56C6C"></el-radio></span>
+            <span style="margin-left:3px"><el-radio :label="none" class="radio"></el-radio></span>
+            <span style="margin-left:3px"><el-radio :label="none" class="radio"></el-radio></span>
+            <span style="margin-left:3px"><el-radio :label="none" class="radio"></el-radio></span>
+          </el-radio-group>
+        </div>
+      </div>
+      <div id="time_data">
+        <div class="title">实时数据</div>
+        <div class="data_circle">
+          <span style="margin-left:10px"><el-progress type="circle" :percentage="25" width="90" stroke-width="10" color="#E6A23C"></el-progress></span>
+          <span style="margin-left:10px"><el-progress type="circle" :percentage="45" width="90" stroke-width="10" color="#409EFF"></el-progress></span>
+          <span style="margin-left:10px"><el-progress type="circle" :percentage="30" width="90" stroke-width="10" color="#F56C6C"></el-progress></span>
+          <span style="margin-left:10px"><el-progress type="circle" :percentage="70" width="90" stroke-width="10" color="#67C23A"></el-progress></span>
+        </div>
+      </div>
+    </div>
+    <div id="right">
+      <div id="video">
+        <el-image :src="videos" style="width:100%;height:100%;border-radius:16px"></el-image>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,16 +44,101 @@
 export default {
   data () {
     return {
-      fieldid: ''
+      fieldid: '',
+      picture: [
+        { id: '1', name: '菜地4' },
+        { id: '2', name: '菜地2' },
+        { id: '3', name: '菜地3' },
+        { id: '4', name: '菜地1' }
+      ],
+      imgs: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2428072447,3963326943&fm=26&gp=0.jpg',
+      videos: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fstc.zjol.com.cn%2Fg1%2FM00250ECggSDloEUziAdZAhAARUVVx6mdQ966.jpg%3Fwidth%3D720%26height%3D540&refer=http%3A%2F%2Fstc.zjol.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614255686&t=ddd4bff2c6061e184c8c99d9abafc323',
+      radio: 4
     }
   },
   created () {
     this.fieldid = this.$route.params.fieldid
   }
-
 }
 </script>
-
 <style scoped>
-
+#right{
+  margin-top:10px;
+  float: left;
+  width: 60%;
+  height: 85%;
+  /* background-color: blue; */
+}
+#left{
+  margin-top:10px;
+  float: left;
+  width: 40%;
+  height: 85%;
+  padding-left:2%;
+  /* background-color: brown; */
+}
+#content{
+  width:90%;
+  height:470px;
+  margin-right:10%;
+  margin-top: 10px;
+  /* background-color: #F56C6C; */
+}
+.data_circle{
+  margin-top: 30px;
+  margin-left: 10px;
+}
+.title{
+  width: 100%;
+  height: auto;
+  margin-top: 10px;
+  padding-left:5px;
+  border-left: 5px #F56C6C solid;
+  font-size: 26px;
+  font-weight: bolder;
+  color: #666666;
+}
+#content_text{
+  width: 100%;
+  height: auto;
+  margin-top: 10px;
+  font-size: 14px;
+  color: #333333;
+  /* background-color: crimson; */
+}
+#content_img{
+  width: 100%;
+  height:250px;
+  /* margin-top: 30px; */
+  /* background-color: darkkhaki; */
+}
+.img{
+  width: 45%;
+  height: 125px;
+  margin: 2%;
+  margin-left: 3%;
+  border-radius: 6px;
+  float: left;
+}
+#img_icon{
+  width: 30%;
+  height: 20px;
+  margin-top: 10px;
+  margin-left:40%;
+  /* background-color: darkcyan; */
+}
+#time_data{
+  width:90%;
+  height:180px;
+  margin-right:10%;
+  margin-top: 10px;
+  /* background-color: bisque; */
+}
+#video{
+  width: 100%;
+  height: 95%;
+  margin-top: 18px;
+  border-radius: 16px;
+  /* background-color:#999999; */
+}
 </style>
