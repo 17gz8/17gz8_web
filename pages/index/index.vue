e0b29e0dad47263814bea78a945682e0ea2909fe
D X <1004369642@qq.com>
2021-02-02 23:58:14 +0800

    Updata: index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -34,10 +34,10 @@ export default {
         { url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg' }
       ],
       gardens: [
-        { text: '体验种植、热爱劳动、热爱生活', url: 'https://image.pearvideo.com/cont/20200926/cont-1699154-12478977.png' },
-        { text: '照顾农作物、关爱生命、热爱自然', url: 'https://res.dutenews.com/a/10001/201903/f426caf43bbef9f6f8e04917347d9d4f.jpg' },
-        { text: '科学实验培养科学兴趣', url: 'http://images.china.cn/attachement/jpg/site1000/20090423/0019b91eca160b5a28c205.jpg' },
-        { text: '团结协作、珍惜劳动成果', url: 'http://5b0988e595225.cdn.sohucs.com/images/20171124/1c579ee70c414880bbf02ef7c1aeb72c.jpeg' }
+        { text: '体验种植、热爱劳动、热爱生活', url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp1.img.cctvpic.com%2Fphotoworkspace%2Fcontentimg%2F2014%2F05%2F03%2F2014050314265431837.jpg&refer=http%3A%2F%2Fp1.img.cctvpic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614873324&t=28fa51217df1be933cb03c81cf9fc605' },
+        { text: '照顾农作物、关爱生命、热爱自然', url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Feducation.news.cn%2F2015-09%2F23%2F128257788_14429652808171n.jpg&refer=http%3A%2F%2Feducation.news.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614873324&t=bdcb534f02157085cfa38a310fdb1ce1' },
+        { text: '科学实验培养科学兴趣', url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fphotocdn.sohu.com%2F20151229%2Fmp51259876_1451402000036_4.jpeg&refer=http%3A%2F%2Fphotocdn.sohu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614873324&t=7ed267765cd97ce4d2de171b3b7a07df' },
+        { text: '团结协作、珍惜劳动成果', url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180626%2Ff6d9d94c6c6b4aca9ad2223051a23d38.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614873324&t=24494515cfa20d1946e4af27b0aa7f73' }
       ],
       fit: 'cover'
     }
