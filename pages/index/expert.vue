c9f916a3cdf9361aa34b4612598b6955e2371a53
cheerddd <2664330029@qq.com>
2021-02-06 19:25:32 +0800

    ‘

--- pages/index/expert.vue
+++ pages/index/expert.vue
@@ -1,50 +1,27 @@
 <template>
-  <el-card class="bigBox">
-    <div class="container">
-      <div class="leftContent">
-        <el-image :src="imgCurrent" class="ex-img" />
-      </div>
-      <div class="rightContent">
-        <div id="experIntro" class="experIntro">
-          <h2>{{ currentExper }}</h2>
-          <h3>{{ currentPosition }}</h3>
-          <p>{{ currentPoint }}</p>
+  <div id="#box" class="container">
+    <el-card v-for="(item,index) in expers[currentIndex]" :key="index">
+      <div class="exper">
+        <div class="left-picture">
+          <el-image :src="item.currentImg" alt="" class="img" />
         </div>
-        <div id="detailIntro" class="detailIntro">
-          <div class="context">
-            <el-image :src="imgUrl2" class="littleimg" />
-            <p>
-              {{ currentIntro }}
-            </p>
-          </div>
-          <div class="imgLists">
-            <el-image :src="img100" class="img100" />
-            <el-image :src="img101" class="img101" />
-            <el-image :src="img100" class="img102" />
-            <el-image :src="img100" class="img103" />
-          </div>
-        </div>
-        <div class="experList">
-          <el-image :src="img009" class="helpImg" />
-          <el-image :src="img010" class="helpText" />
-          <div class="experImgs">
-            <div class="mainlyExper">
-              <el-image :src="img007" class="helpLeft" />
-              <div v-for="(item,index) in imgs" :key="index" class="imgs">
-                <el-image :src="item.url" class="exper01" />
-              </div>
-              <el-image id="btn" :src="img008" class="helpRight" @click="handle" />
-            </div>
-            <div id="allExper" class="allExpers">
-              <div v-for="(item,index) in allImgs" :key="index" class="Expers">
-                <el-image :src="item.url" class="expers" />
-              </div>
-            </div>
-          </div>
+        <div class="right-intro">
+          <p class="name">
+            {{ item.name }}
+          </p>
+          <p class="position">
+            {{ item.position }}
+          </p>
+          <p class="point">
+            {{ item.point }}
+          </p>
+          <p class="intro">
+            {{ item.intro }}
+          </p>
         </div>
       </div>
-    </div>
-  </el-card>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
@@ -56,294 +33,147 @@ export default {
   },
   data () {
     return {
-      // 当前大图显示的专家信息
-      currentExper: '汪明荃',
-      currentPosition: '陕西农业大学教授',
-      currentPoint: '农作物的生长特效，病理研究',
-      currentIntro: '现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士',
-      imgCurrent: require('../../static/images/001.jpg'),
-      imgUrl2: require('../../static/images/02.png'),
-      img009: require('../../static/images/009.png'),
-      img010: require('../../static/images/010.png'),
-      img007: require('../../static/images/007.png'),
-      img003: require('../../static/images/003.png'),
-      img004: require('../../static/images/004.png'),
-      img005: require('../../static/images/00x.png'),
-      img008: require('../../static/images/008.png'),
-      img100: require('../../static/images/100.png'),
-      img101: require('../../static/images/101.png'),
-      // 右下专家列表的专家图片
-      imgs: [
-        { url: require('../../static/images/006.png') },
-        { url: require('../../static/images/004.png') },
-        { url: require('../../static/images/005.png') },
-        { url: require('../../static/images/003.png') }
-      ],
-      // 隐藏的专家列表图片
-      allImgs: [
-        { url: require('../../static/images/006.png') },
-        { url: require('../../static/images/004.png') },
-        { url: require('../../static/images/00x.png') },
-        { url: require('../../static/images/003.png') },
-        { url: require('../../static/images/006.png') },
-        { url: require('../../static/images/004.png') },
-        { url: require('../../static/images/00x.png') },
-        { url: require('../../static/images/003.png') }
-      ]
-    }
-  },
-  methods: {
-    handle () {
-      const btn = document.getElementById('allExper')
-      btn.style.display = 'flex'
+      currentIndex: 0,
+      expers: {
+        0: [
+          {
+            id: '1',
+            currentImg: require('../../static/images/001.jpg'),
+            name: '汪明荃',
+            position: '陕西农业大学教授',
+            point: '农作物的生长特效，病理研究',
+            intro: '现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士'
+          },
+          {
+            id: '2',
+            currentImg: require('../../static/images/001.jpg'),
+            name: '汪明荃',
+            position: '陕西农业大学教授',
+            point: '农作物的生长特效，病理研究',
+            intro: '现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士'
+          },
+          {
+            id: '3',
+            currentImg: require('../../static/images/001.jpg'),
+            name: '汪明荃',
+            position: '陕西农业大学教授',
+            point: '农作物的生长特效，病理研究',
+            intro: '现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士'
+          },
+          {
+            id: '1',
+            currentImg: require('../../static/images/001.jpg'),
+            name: '汪明荃',
+            position: '陕西农业大学教授',
+            point: '农作物的生长特效，病理研究',
+            intro: '现任山东省医学科学院名誉院长、山东省科协副主席、山东省眼科研究所名誉所长、省部共建眼科学国家重点实验室培育基地主任。兼任中华医学会和中国医师协会眼科学分会荣誉主任委员、亚洲角膜学会副主席、国际眼表疾病学会理事、国际泪膜与眼表学会学术委员会委员和亚太接触镜眼科医师协会理事等，是目前我国眼科学界唯一的中国工程院院士'
+          }
+        ]
+      }
     }
   }
 }
 </script>
 <style scoped>
 /* 大卡片 */
-.bigBox{
-  width:90%;
-  margin:2% 5%;
-  height:100%;
-  min-width:250px;
+template{
+  min-width:600px;
+}
+#box{
+  min-width:600px;
 }
 .container{
-  position: relative;
-  width:100%;
-  height:96%;
-  /* padding:20px 200px 40px 150px; */
-  padding:5% 5%;
-  display: flex;
+  padding:2% 2%;
+  display:flex;
   flex-direction: row;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  flex-wrap: wrap
 }
-/* 左侧图片 */
-.leftContent{
+.el-card{
+  height:350px;
+  display:flex;
+  width:75%;
+  margin-bottom:2rem;
+  padding:0 1rem;
+}
+.exper{
+  width:100%;
+  height:100%;
+  display:flex;
+  flex-direction:row;
+  justify-content: center;
+  align-items: center;
+  /* column-gap:1rem; */
+}
+.left-picture{
   flex:1;
-   /* margin-left:200px; */
-  height:100%;
-  width:100%;
-}
-/* 专家大图片 */
-.ex-img{
-  width:100%;
   height:100%;
 }
-/* 右侧介绍部分 */
-.rightContent{
+.img{
+  width:100%;
+  height:100%;
+  object-fit:cover;
+}
+.right-intro{
   flex:1.2;
-  height:100%;
   display: flex;
   flex-direction: column;
-  justify-content:flex-start;
-  align-items:space-around;
-   /* margin-left:50px; */
-}
-/* 介绍当前专家标题 */
-.experIntro{
-flex:1;
-width:100%;
-margin-bottom:10px;
-position: relative;
-}
-.experIntro h2{
-  width:100%;
   color:#666666;
+  /* justify-content: space-between;
+  align-items: space-around; */
+}
+.right-intro .name{
+  flex:1;
+  font-size:24px;
+  margin-bottom:2%;
+  color:#666666;
+  font-weight:600;
   font:Source Han Sans CN bold 80px;
   border-left:5px #FF6262 solid;
   padding:0 1%;
-  position:relative;
-  left:-110px;
-  text-align:left;
 }
-.experIntro h3{
-  color:#666666;
-  font:Source Han Sans CN lighter 48px;
-  font-weight:lighter;
-  line-height:48px;
-  text-align:left;
-}
-.experIntro p{
-  color:#666666;
-  font:Source Han Sans CN lighter 24px;
-  font-weight:lighter;
-  line-height:24px;
-  text-align:left;
-}
-/* 当前专家详细介绍 */
-.detailIntro{
+.right-intro .position{
   flex:1;
-  width:100%;
-  color:#666666;
-  font:Source Han Sans CN lighter 24px;
-  font-weight:lighter;
-  line-height:41px;
-  display:flex;
-  flex-direction: row;
+  margin-bottom:2%;
+  font-weight:600;
 }
-.detailIntro .context{
-  flex:7;
-}
-.detailIntro .imgLists{
+.right-intro .point{
   flex:1;
-  height:120px;
-  display:flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  transform: translateY(90px);
+  margin-bottom:2%;
+  font-weight:600;
 }
-.detailIntro .imgLists .img100,.detailIntro .imgLists .img101{
-  width:24px;
-  height:24px;
-}
-.detailIntro .imgLists .img102,.detailIntro .imgLists .img103{
-  width:10px;
-  height:10px;
-}
-.littleimg{
-  width:8%;
-  height:9%;
-  margin-bottom:10px;
-}
-/* 右侧下方专家列表 */
-.experList{
-  flex:1;
-  width:100%;
-  margin-top:10px;
-}
-.experList .helpImg{
-  width:12%;
-  height:20%;
-}
-.experList .helpText{
-  width:12%;
-  height:8%;
-  position: relative;
-  top:-3%;
-}
-.experList .experImgs{
-  width:100%;
-  display:flex;
-  flex-direction: column;
-  justify-content:center;
-  align-items:center;
-}
-/* 当前显示的专家列表 */
-.experList .experImgs .mainlyExper{
-  flex:1;
-  transform:translateX(-3%);
-  width:100%;
-  display:flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  column-gap: 3%;
-  margin:20px 0 10px 0;
-  row-gap:1%;
-}
-/* 触发点击事件前隐藏的专家列表 */
-.experList .experImgs .allExpers{
-  flex:2;
-  width:100%;
-  /* display:flex; */
-  display:none;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: wrap;
-  column-gap: 3%;
-  /* row-gap:4%; */
-  transform:translateX(4%);
-}
-.experList .experImgs .allExpers .Expers{
-  width:19%;
-  margin-bottom:10px;
-}
-.helpLeft,.helpRight{
-  flex:1;
-  cursor:pointer;
-}
-.experList .experImgs .imgs{
-  flex:4;
-  display:flex;
-  flex-direction:row;
-  cursor:pointer;
+.right-intro .intro{
+  flex:6;
+  margin-bottom:2%;
 }
 /* 媒体查询 */
-@media screen and (max-width: 1000px){
-  .rightContent h2{
-    left:-80px;
+@media screen and (max-width: 900px){
+  .container{
+  padding:1px;
   }
-}
-@media screen and (max-width: 870px)
-{
-  .bigBox{
-    height:60%;
-  }
-  .ex-img{
-    width:100%;
-    height:100%;
-  }
-  .rightContent{
-    flex:1;
-  }
-  .rightContent h2{
-    left:-70px;
-  }
-  .detailIntro{
-  font:Source Han Sans CN lighter 20px;
-  line-height:20px;
-}
-}
-@media screen and (max-width: 800px){
-  .rightContent h2{
-    left:-60px;
+  .el-card{
+  width:95%;
   }
 }
 @media screen and (max-width: 700px){
-  .rightContent h2{
-    left:0px;
-  }
-  .detailIntro{
-  font:Source Han Sans CN lighter 18px;
-  line-height:18px;
+  .right-intro .name{
+  font-size:18px;
+  margin-bottom:2%;
+  color:#666666;
+  font-weight:600;
+  font:Source Han Sans CN bold 80px;
+  border-left:5px #FF6262 solid;
+  padding:0 1%;
 }
-.littleimg{
-  width:8%;
-  height:5%;
-  margin-bottom:10px;
+.right-intro .position{
+  font-size:14px;
 }
+.right-intro .point{
+  font-size:14px;
 }
-@media screen and (max-width: 600px){
-  .rightContent h2{
-    left:0px;
-  }
-  .detailIntro{
-  font:Source Han Sans CN lighter 12px;
-  line-height:16px;
-}
-.littleimg{
-  width:6%;
-  height:5%;
-  margin-bottom:10px;
+.right-intro .intro{
+  font-size:14px;
 }
 }
-@media screen and (max-width: 500px){
-  .container{
-  flex-direction: column;
-}
-.littleimg{
-  width:6%;
-  height:6%;
-  margin-bottom:10px;
-}
-.detailIntro{
-  font:Source Han Sans CN lighter 18px;
-  line-height:18px;
-}
-}
-/* 媒体查询（自适应）结束 */
 </style>
